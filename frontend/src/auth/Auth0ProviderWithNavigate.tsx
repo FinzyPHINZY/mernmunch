c1f975ae255d5afcbe1f9102ae823edfd3135fda
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-07-25 11:54:43 +0100

    feat: create user request from frontend and setup React Query
    - Added useCreateMyUser hook to handle user creation API requests
    - Configured React Query for managing API request states
    - Implemented onRedirectCallback in Auth0ProviderWithNavigate to create user in DB on authentication

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,6 +1,13 @@
 import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+import useCreateMyUser from "../api/UserApi";
 
-function Auth0ProviderWithNavigate({ children }: Props) {
+type Props = {
+  children: React.ReactNode;
+};
+
+const Auth0ProviderWithNavigate = ({ children }: Props) => {
+  const { createUser } = useCreateMyUser();
+
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
@@ -9,7 +16,11 @@ function Auth0ProviderWithNavigate({ children }: Props) {
     throw new Error("ERROR: Failed to initialize auth");
   }
 
-  const onRedirectCallback = (appState?: AppState, user?: User) => {};
+  const onRedirectCallback = (appState?: AppState, user?: User) => {
+    if (user?.sub && user?.email) {
+      createUser({ auth0Id: user.sub, email: user.email });
+    }
+  };
 
   return (
     <Auth0Provider
@@ -21,10 +32,6 @@ function Auth0ProviderWithNavigate({ children }: Props) {
       {children}
     </Auth0Provider>
   );
-}
-
-type Props = {
-  children: React.ReactNode;
 };
 
 export default Auth0ProviderWithNavigate;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,13 +4,25 @@ import "./global.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./AppRoutes";
 import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      // refetchInterval: 30000, // 30 seconds
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
-      <Auth0ProviderWithNavigate>
-        <AppRoutes />
-      </Auth0ProviderWithNavigate>
+      <QueryClientProvider client={client}>
+        <Auth0ProviderWithNavigate>
+          <AppRoutes />
+        </Auth0ProviderWithNavigate>
+      </QueryClientProvider>
     </Router>
   </React.StrictMode>
 );

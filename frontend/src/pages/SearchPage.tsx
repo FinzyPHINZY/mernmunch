b891ea77d0c3768bd81a637cd188b98ea0dd13d3
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-08-04 08:03:03 +0100

    feat: build SearchPage with restaurant search results and associated components
    - Implemented `SearchPage` to display restaurants matching the search query:
      - Integrated `SearchResultCard` component to showcase each restaurant's information, including:
        - Restaurant banner image, name, cuisines, estimated delivery time, and delivery price.
        - Designed the card layout with a responsive grid and hover effects for improved user experience.
    
      - Developed `SearchResultInfo` component:
        - Displays the total number of restaurants found in a specific city.
        - Includes a link to change the location and placeholder for a future sort dropdown feature.
    
    - Added routing for `SearchPage` to ensure seamless navigation based on user search queries.
    
    - Ensured the design is responsive and user-friendly, providing a clean and informative presentation of search results.
    
    This implementation enhances the search functionality by providing users with detailed and visually appealing information about available restaurants, improving the overall user experience.

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,21 +1,31 @@
 import { useParams } from "react-router-dom";
 import { useSearchRestaurants } from "../api/RestaurantApi";
+import SearchResultInfo from "../components/SearchResultInfo";
+import SearchResultCard from "../components/SearchResultCard";
 
 const SearchPage = () => {
   const { city } = useParams();
+  const { results, isLoading } = useSearchRestaurants(city);
 
-  const { results } = useSearchRestaurants(city);
+  if (isLoading) {
+    return <span>Loading...</span>;
+  }
+
+  if (!results?.data || !city) {
+    return <span>No results found</span>;
+  }
 
   return (
-    <div>
-      User searched for {city}
-      <span>
-        {results?.data.map((restaurant, i) => (
-          <p>
-            {i + 1} - {restaurant.restaurantName}, {restaurant.city}
-          </p>
+    <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
+      <div className="" id="cuisines-list">
+        Insert cuisines here:{" "}
+      </div>
+      <div className="flex flex-col gap-5" id="main-content">
+        <SearchResultInfo city={city} total={results.pagination.total} />
+        {results.data.map((restaurant) => (
+          <SearchResultCard restaurant={restaurant} />
         ))}
-      </span>
+      </div>
     </div>
   );
 };

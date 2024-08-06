38f13e9604131b15f7c052d02c7d600a106cb0ed
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-08-06 15:35:45 +0100

    Update addToCart and removeFromCart functions to store cart items in session storage
    - Modified addToCart and removeFromCart to save updated cart items in sessionStorage with key based on restaurant ID.
    - Initialized cartItems state with items from sessionStorage, if available.
    - Built CheckoutButton component to handle user authentication and login redirect using Auth0.

--- frontend/src/pages/DetailPage.tsx
+++ frontend/src/pages/DetailPage.tsx
@@ -4,9 +4,10 @@ import { AspectRatio } from "../components/ui/aspect-ratio";
 import RestaurantInfo from "../components/RestaurantInfo";
 import MenuItem from "../components/MenuItem";
 import { useState } from "react";
-import { Card } from "../components/ui/card";
+import { Card, CardFooter } from "../components/ui/card";
 import OrderSummary from "../components/OrderSummary";
 import { MenuItem as MenuItemType } from "../types";
+import CheckoutButton from "../components/CheckoutButton";
 
 export type CartItem = {
   _id: string;
@@ -18,7 +19,10 @@ export type CartItem = {
 const DetailPage = () => {
   const { restaurantId } = useParams();
   const { restaurant, isLoading } = useGetRestaurant(restaurantId);
-  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+  const [cartItems, setCartItems] = useState<CartItem[]>(() => {
+    const storedCartItems = sessionStorage.getItem(`cartItems-${restaurantId}`);
+    return storedCartItems ? JSON.parse(storedCartItems) : [];
+  });
 
   const addToCart = (menuItem: MenuItemType) => {
     setCartItems((prevCartItems) => {
@@ -45,6 +49,11 @@ const DetailPage = () => {
           },
         ];
       }
+
+      sessionStorage.setItem(
+        `cartItems-${restaurantId}`,
+        JSON.stringify(updatedCartItems)
+      );
       return updatedCartItems;
     });
   };
@@ -53,6 +62,11 @@ const DetailPage = () => {
     setCartItems((prevCartItems) => {
       const updatedCartItems = prevCartItems.filter(
         (item) => cartItem._id !== item._id
+      );
+
+      sessionStorage.setItem(
+        `cartItems-${restaurantId}`,
+        JSON.stringify(updatedCartItems)
       );
 
       return updatedCartItems;
@@ -95,6 +109,9 @@ const DetailPage = () => {
               cartItems={cartItems}
               removeFromCart={removeFromCart}
             />
+            <CardFooter>
+              <CheckoutButton />
+            </CardFooter>
           </Card>
         </div>
       </div>
